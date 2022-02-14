1b8c110e0b835b96921cbd1dc11f7c483651c39a
David Clark <david.clark.1043@gmail.com>
2022-02-14 16:02:51 -0600

    missed playerform in previous commit

--- src/scripts/player/PlayerForm.js
+++ src/scripts/player/PlayerForm.js
@@ -1,8 +1,17 @@
 // import teamDropdown
+// import PlayerCount
+// import getTeams
 // import sendPlayer
 
 // function that generates html for the player form
+// data from form is:
+    // player name
+    // team from dropdown
 
 // add eventListener that 
     // gets data from the form
-    // adds player to API via sendPlayer 
+    // check if team is full
+        // if already 3 players on team
+        // don't send anything and send alert to browser
+    // else
+        // adds player to API via sendPlayer 
