e778377381e614196f8a043494e855b31c77222c
Noah Taggart <noahtag@gmail.com>
2022-02-15 14:21:39 -0600

    player modules

--- src/scripts/player/PlayerCount.js
+++ src/scripts/player/PlayerCount.js
@@ -1,8 +1,15 @@
 // import getPlayers
-// import getTeams
+import { getPlayers } from "./PlayerProvider.js";
+
 
 // export PlayerCount(teamId) function calculate number of players on each team
+export const PlayerCount = (teamId) => {
+    const players = getPlayers()
     // for each team
         // filter players for teamId
-        // .length
+    const matchedPlayers = players.filter(player => player.teamId === teamId)
     // returns numberOfPlayers
+            // .length
+    return matchedPlayers.length
+}
+
