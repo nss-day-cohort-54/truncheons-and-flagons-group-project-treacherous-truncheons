76f7c1dc025a824c680f0ffa89ebcf221919c7db
David Clark <david.clark.1043@gmail.com>
2022-02-15 13:40:17 -0600

    Added data functions for score and team providers

--- src/scripts/score/ScoreProvider.js
+++ src/scripts/score/ScoreProvider.js
@@ -1,7 +1,43 @@
+import { API, applicationState } from "../dataAccess.js";
+
 // export fetchScores - gets score data from api
+// gets score list from api, saves it to the applicationState
+export const fetchScores = () => {
+    return fetch(`${API}/scores`)
+        .then(response => response.json())
+        .then(
+            (scores) => {
+                // Store the external state in application state
+                applicationState.scores = scores
+            }
+        )
+}
 
 // export sendScore - add score data to api
+// takes a score object and sends it to the api
+export const sendScore = (score) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(score)
+    }
+
+
+    return fetch(`${API}/scores`, fetchOptions)
+        .then(response => response.json())
+}
 
 // export getScores - gets team score data from applicationState
+// returns a copy of score list from applicationState
+export const getScores = () => {
+    return applicationState.scores.map(score => ({...score}))
+}
 
 // export setScore(teamId, score) - add input to that teams respective value in gameState
+
+export const setScore = (teamId, score) => {
+    applicationState.gameState[teamId] += score
+    return ""
+}
--- src/scripts/team/TeamProvider.js
+++ src/scripts/team/TeamProvider.js
@@ -1,5 +1,36 @@
+import { API, applicationState } from "../dataAccess.js";
+
 //export fetchTeams
+// gets team list from api, saves it to the applicationState
+export const fetchTeams = () => {
+    return fetch(`${API}/teams`)
+        .then(response => response.json())
+        .then(
+            (teams) => {
+                // Store the external state in application state
+                applicationState.teams = teams
+            }
+        )
+}
 
 //export sendTeams
+// takes a team object and sends it to the api
+export const sendTeam = (team) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(team)
+    }
+
+
+    return fetch(`${API}/teams`, fetchOptions)
+        .then(response => response.json())
+}
 
 //export getTeams
+// returns a copy of team list from applicationState
+export const getTeams = () => {
+    return applicationState.teams.map(team => ({...team}))
+}
