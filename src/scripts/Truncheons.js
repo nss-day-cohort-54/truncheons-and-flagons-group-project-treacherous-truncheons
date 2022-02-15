6885a0b1602d8f5d555afe253ffacf92113bdcf1
David Clark <david.clark.1043@gmail.com>
2022-02-15 14:36:48 -0600

    Added html skeleton

--- src/scripts/Truncheons.js
+++ src/scripts/Truncheons.js
@@ -1,4 +1,37 @@
 //import Game, playerForm, teamForm, leaderboard html functions
+import { Game } from "./game/Game.js";
+import { TeamForm } from "./team/TeamForm.js";
+import { Leaderboard } from "./Leaderboard.js";
+import { PlayerForm } from "./player/PlayerForm.js";
+
 
 //export function that interpolates entire html string
+export const TruncheonsFlagons = () => {
+    let htmlString = ""
+
+    // add new team component
+    htmlString +=  `<article class="newTeam">
+                        ${TeamForm()}
+                    </article>
+    `
+
+    // add new player component
+    htmlString += `<article class="newPlayer">
+                        ${PlayerForm()}
+                    </article>
+    `
+
+    // add game component
+    htmlString += `<article class="gameArea">
+                        ${Game()}
+                    </article>
+    `
+    // add leaderboard component
+    htmlString += `<article class="leaderboard">
+                        ${Leaderboard()}
+                    </article>
+    `
+
     //return html string
+    return htmlString
+}
