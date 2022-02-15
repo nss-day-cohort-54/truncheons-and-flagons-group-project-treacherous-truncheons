299c55fd9843d2a4c4edfb67c0b7bf4a9f7a2805
David Clark <david.clark.1043@gmail.com>
2022-02-15 13:47:20 -0600

    Added ability to test functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
-//import fetches, Truncheons function
+// import fetches, Truncheons function
+// import { applicationState } from "./dataAccess.js";
+import { fetchScores } from "./score/ScoreProvider.js";
+import { fetchTeams } from "./team/TeamProvider.js";
+
+const mainContainer = document.querySelector("#container")
+
+const renderAll = () => {
 //invokes fetches
+    const fetchArray = [ fetchScores(), fetchTeams() ]
+    return Promise.all(fetchArray)
+            .then(() => {
+                // renders html
+                // test with just raw applicationState
+                // mainContainer.innerHTML = JSON.stringify(applicationState)
+            })
+}
+
 //render html
+renderAll()
+
+// eventListener to re-render html on state change
+document.addEventListener("stateChanged", event => {
+    renderAllHTML()
+})
