361c24ff3e791515d1ef303653ead209beb6b00b
David Clark <david.clark.1043@gmail.com>
2022-02-16 11:44:41 -0600

    Adding game buttons and logic

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,4 +1,6 @@
 //import teamdropdown function, TeamScoreInput function
+import { getGameState } from "./GameProvider.js";
+import { StartGameButton } from "./StartGameButton.js";
 import { TeamScoreInput } from "./TeamScoreInput.js";
 /*
     Responsibility: Generate HTML for the game component
@@ -7,21 +9,38 @@ import { TeamScoreInput } from "./TeamScoreInput.js";
 // function that generates HTML
 export const Game = () => {
     // get gameState
+    const gameState = getGameState()
     let gameHTML = ""
     // if gameState has roundNumber property
+    if("roundNumber" in gameState) {
         // if roundNumber = 0
+        if(gameState.roundNumber === 0){
+            // make header text for team select view
+            gameHTML += `<div class="selectTeamHeader">Please Select Teams for New Game</div>`
+            
             // invoke dropdown menu function(line 16)
-        // else
+            gameHTML += `${gameTeamOptions()}`
+
+        } else {
+            // add round info
+            gameHTML += `<div class="roundNumber">Round ${gameState.roundNumber}</div>`
+            
             // invoke TeamScoreInput function
-    gameHTML += `${TeamScoreInput()}`
-    // else
+            gameHTML += `${TeamScoreInput()}`
+            
+            // invoke save round score button
+            gameHTML += `${RoundScoreButton()}`
+
+        }
+    } else {
         // generate the start game button
+        gameHTML += `${StartGameButton()}`
+    }
     return gameHTML
 }
 
-// add click eventlistener to the start game button
-    //dispatch statechanged event
 // add eventlistener to the teamDropdown
+
 
 // click eventlistener on save round score button
     // each round add each team score
