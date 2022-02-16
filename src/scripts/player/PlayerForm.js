4218a4fc5d9dc79dd573ee3d13c7723e85afeac5
Noah Taggart <noahtag@gmail.com>
2022-02-16 09:57:26 -0600

    working new player feature

--- src/scripts/player/PlayerForm.js
+++ src/scripts/player/PlayerForm.js
@@ -1,10 +1,8 @@
 // import teamDropdown
-//import { teamDropdown } from "../TeamDropDown.js"
+//import { allTeams } from "../TeamDropDown.js"
 // import PlayerCount
-import { sendPlayers } from "./PlayerProvider.js"
-import { PlayerCount} from "./PlayerCount.js"
-//import applicationState
-import { applicationState } from "../dataAccess.js"
+import { resetPlayerTeam, sendPlayers } from "./PlayerProvider.js"
+import { PlayerCount } from "./PlayerCount.js"
 import { setPlayerTeam } from "./PlayerSelectedTeam.js"
 
 
@@ -13,19 +11,29 @@ export const PlayerForm = () => {
     //saves imports as variables
     //formated team list
     let teamList =
-        `<select name="teams"><option name="teams" value="0">Please select a team...</option><option name="teams" value="1">Team 1</option>`
+        `<select name="teams"><option value="0">Please select a team...</option><option value="1">Team 1</option>`
 
-    //teamList += `${teamDropdown()}`
+    //teamList += `${allTeams()}`
     teamList += `<select>`
 
     let html = `
-        <div class="inputField">
+        <div id="inputField">
             <label class="label" for="playerName"></label>
             <input type="text" name="playerName" class="input" >
             ${teamList}
         </div>
         <button class="button" id="submitPlayer">Add Player to the Team</button>
         `
+
+
+
+
+
+
+
+
+
+
     // data from form is:
     // define html string that has a label and input field
     // interpolate html that invokes function from TeamDropdown and add to html string
@@ -33,15 +41,6 @@ export const PlayerForm = () => {
     return html
 }
 
-//eventlistener to check for team to be selected
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.name === "teams") {
-            setPlayerTeam(parseInt(event.target.value))
-        }
-    }
-)
 
 
 // check if team is full
@@ -58,23 +57,23 @@ document.addEventListener(
         if (clickEvent.target.id === "submitPlayer") {
             // gets data from the form
             const name = document.querySelector("input[name=playerName]").value
-            const playerTeam = applicationState.playerTeam
+            const playerTeam = parseInt(document.querySelector("select[name='teams']").value)
             // invoke playerCount with the id of whichever team was clicked on as the parameter
             const playerCount = PlayerCount(playerTeam)
-            if (playerCount && playerCount < 3) {
+            if (playerCount === 0) {
+                window.alert("Please select a team for your player.")
+            } else if (playerCount >= 3) {
+                window.alert("Selected team is full. Please select another.")
+            } else {
                 const playerData = {
                     fullName: name,
                     teamId: playerTeam
                 }
                 sendPlayers(playerData)
-                .then(() => setPlayerTeam(null))
-            } else {
-                window.alert("Selected team is full. Please select another.")
-                setPlayerTeam(null)
+                    .then(() => document.dispatchEvent (new CustomEvent("stateChanged")))
             }
         }
-    }
-)
+    })
 
 
 //look into saving text input as state
