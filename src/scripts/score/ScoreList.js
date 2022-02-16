28aee18402bfc5a1bd8566683ac481390fea8b60
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-16 10:52:04 -0600

    Defined scoreList function that filters through teams and prints a row with their respective player numbers and total score

--- src/scripts/score/ScoreList.js
+++ src/scripts/score/ScoreList.js
@@ -1,16 +1,22 @@
 //import getTeams, totalScore, import playerCount
-import { getTeams } from "../team/TeamProvider";
-import { totalScore} from "./totalScore.js";
+import { getTeams } from "../team/TeamProvider.js";
+import { totalScore } from "./totalScore.js";
+import { PlayerCount } from "../player/PlayerCount.js";
 
 //define function that iterates over teams
 export const LeaderBoard = () => {
     const teams = getTeams()
-    let html = ``
+    let html = `<tr>`
 
-    for (const team of teams) {
-
-    }
-}
     //interpolate string for each team that displays team name, totalScore, and playerCount
+    for (const team of teams) {
+        html += `<td>${team.teamName}</td>
+                 <td>${PlayerCount(team.id)}</td>
+                 <td>${totalScore(team.id)}</td>`
+    }
+    html += `</tr>`
     //return html
+    
+    return html
+}
 
