9c4f2d1521fb44b917970473aa47c0742caedd4f
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-16 13:32:14 -0600

    fixed my leaderboard function to sort before creating the html

--- src/scripts/score/ScoreList.js
+++ src/scripts/score/ScoreList.js
@@ -3,27 +3,21 @@ import { getTeams } from "../team/TeamProvider.js";
 import { totalScore } from "./totalScore.js";
 import { PlayerCount } from "../player/PlayerCount.js";
 
-//sort all totalScores so the leaderboard is displayed in the right order
-const sortTotalScores = () => {
-    const teams = getTeams()
-    //define an empty array
-    const scoreArr = []
-
-    for (const team of teams) {
-        //for each team add their totalScore to that array
-        scoreArr.push(totalScore(team.id))
-    }
-
-    const sortedScores = scoreArr.sort(( score1, score2) => score2 - score1)
-    return sortedScores
-}
 
 
 //define function that iterates over teams
 export const TeamRows = () => {
     const teams = getTeams()
+    //make a copy of teams array and save their totalScore as a property
+    const totalTeamScores = teams.map((team) => ({
+        ...team,
+        totalScore: totalScore(team.id)
+    }))
+    
+    //sort all teams by their totalScore property so the leaderboard is displayed in the right order
+    const sortedTeams = totalTeamScores.sort(( team1, team2) => team2.totalScore - team1.totalScore)
     let html = ""
-        
+        for (const team of sortedTeams)
         //interpolate string for each team that displays team name, totalScore, and playerCount
         html += `<tr>
                     <td>${team.teamName}</td>
