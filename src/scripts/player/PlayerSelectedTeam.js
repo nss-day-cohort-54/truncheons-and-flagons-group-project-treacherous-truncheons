ce20a185eb52fe13d6de9994676c3d00939051b2
Noah Taggart <96712524+noahtaggart@users.noreply.github.com>
2022-02-16 10:01:59 -0600

    Delete PlayerSelectedTeam.js

--- src/scripts/player/PlayerSelectedTeam.js
+++
@@ -1,6 +0,0 @@
-import {applicationState} from "../dataAccess.js"
-
-export const setPlayerTeam = (teamId) => {
-    applicationState.playerTeam = teamId
-    document.dispatchEvent (new CustomEvent("stateChanged"))
-}
